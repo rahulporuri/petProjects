f2ca638c54974bb6b7d907137752f5e8162fd8e2
rahul.poruri <rahul.poruri@gmail.com>
2016-01-13 12:04:49 +0530

    used groups and cleaned up stupid bug

--- models.py
+++ models.py
@@ -8,25 +8,28 @@ step = 0.01			# step size for the rk4 stepper
 m = 1.				# mass of the object undergoing 
 				# damped driven harmonic oscillations
 
-h=6.6244e-34 # Planck constant in Js
-c=2.998e+08     # speed of light in m/s
-kb=1.38e-23    #Boltzmann constant in J/K%
-
+h = 6.6244e-34 # Planck constant in Js
+c = 2.998e+08     # speed of light in m/s
+kb = 1.38e-23    #Boltzmann constant in J/K%
 
 def ddx(k, m, a, x, dx):
 	"""function that estimates the 
 	second derivative of the position x"""
 	return -k*x/m -a*dx/m
 
-def rk2_step(k, m, a, x, dx):
+def rk4_step(k, m, a, x, dx):
 	"""function that updates the position x
 	using a runge-kutta 2 method"""
 	F1 = dx
 	f1 = ddx(k, m, a, x, dx)
-	F2 = dx +f1*step
-	f2 = ddx(k, m, a, x +F1*step, dx +f1*step)
-	
-	return (f1 +f2)*step/2., (F1 +F2)*step/2. 
+	F2 = dx +f1*step/2.
+	f2 = ddx(k, m, a, x +F1*step/2., dx +f1*step/2.)
+	F3 = dx +f2*step/2.
+	f3 = ddx(k, m, a, x +F2*step/2., dx +f2*step/2.)
+	F4 = dx +f3*step
+	f4 = ddx(k, m, a, x +F3*step, dx +f3*step)
+
+	return (f1 +2*f2 +2*f3 +f4)*step/6., (F1 +2*F2 +2*F3 +F4)*step/6. 
 	# (dy, y) update
 
 class Models(HasTraits):
@@ -65,7 +68,7 @@ class Models(HasTraits):
 		i = 0
 		while i < len(t):
 			x.append(xi)
-			dxi_inc, xi_inc = rk2_step(k,m,a,xi,dxi)
+			dxi_inc, xi_inc = rk4_step(k,m,a,xi,dxi)
 			dxi += dxi_inc
 			xi += xi_inc
 			i += 1
@@ -103,7 +106,7 @@ class Models(HasTraits):
 		"""checks for changes in a 
 		and updates the plot corrspondingly"""
 		print 'the damping parameter : a has been changed from %s to %s ' % (old, new)
-		self.eval_bbrad_plot()                               
+		self.eval_ddho_plot()                               
 
         def _T_changed(self, old, new):
                 """checks for changes in k 
@@ -111,12 +114,17 @@ class Models(HasTraits):
                 print 'the temperature of the blackbody emitter : T has been changed from %s to %s ' % (old, new)
                 self.eval_bbrad_plot()
 
-view1 = View(Group(Item(name = 'disp_bbrad'),
-		     Item(name = 'T', enabled_when = 'disp_bbrad == True')),
-		Group(Item(name = 'disp_ddho'),
-	             Item(name = 'k', enabled_when = 'disp_ddho == True'),
-	             Item(name = 'a', enabled_when = 'disp_ddho == True')),
-)
+view1 = View(Item(name = 'disp_bbrad'),
+	     Item(name = 'T', enabled_when = 'disp_bbrad == True'),
+	     Item(name = 'disp_ddho'),
+	     Item(name = 'k', enabled_when = 'disp_ddho == True'),
+	     Item(name = 'a', enabled_when = 'disp_ddho == True'),
+	)
+
+view2 = View(Group(Item(name = 'T')),
+		Group(Item(name = 'k'),
+	             Item(name = 'a')),
+	)
 
 model_inst = Models()
-model_inst.configure_traits(view = view1)
+model_inst.configure_traits(view = view2)
