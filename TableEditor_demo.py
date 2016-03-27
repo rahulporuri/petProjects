a0c63b848d06ebdfbd32aa1e5b638bf3e37ff82b
Poruri Sai Rahul <rahul.poruri@gmail.com>
2016-03-27 10:40:38 +0530

    Update TableEditor_demo.py

--- TableEditor_demo.py
+++ TableEditor_demo.py
@@ -14,7 +14,7 @@ from numpy \
     import linspace, sin
 
 from traits.api \
-    import HasTraits, HasStrictTraits, Instance, Str, Button, Float, List, on_trait_change
+    import HasTraits, HasStrictTraits, Instance, Int, Str, Button, Float, List, on_trait_change
 
 from traitsui.api \
     import View, Group, VGroup, HGroup, Item, TableEditor 
@@ -23,29 +23,25 @@ from traitsui.table_column \
     import ObjectColumn 
 
 from chaco.api \
-    import ArrayPlotData, HPlotContainer, Plot
+    import ArrayPlotData, HPlotContainer, Plot, ArrayDataSource, DataRange1D, LinearMapper, BarPlot, PlotGraphicsContext
+
+from chaco.tools.api \
+    import SaveTool
 
 from enable.component_editor \
     import ComponentEditor
 
-# A helper class for the 'Department' class below:
+# A helper class for the 'Application' class below:
 class row_data( HasTraits ):
-    first_name = Str
-    last_name  = Str
-    age        = Float 
-
-    traits_view = View(
-        'first_name', 'last_name', 'age', 'phone',
-        title   = 'Create new employee',
-        width   = 0.18,
-        buttons = [ 'OK', 'Cancel' ]
-    )
+    x = Int
+    y = Float
+    z = Float 
 
 # The definition of the demo TableEditor:
 table_editor = TableEditor(
-    columns = [ ObjectColumn( name = 'first_name', width = 0.20 ),
-                ObjectColumn( name = 'last_name',  width = 0.20 ),
-                ObjectColumn( name = 'age',   width = 0.10,
+    columns = [ ObjectColumn( name = 'x', width = 0.20 ),
+                ObjectColumn( name = 'y',  width = 0.20 ),
+                ObjectColumn( name = 'z',   width = 0.10,
                               horizontal_alignment = 'center' ),
               ],
     deletable   = True,
@@ -56,46 +52,78 @@ table_editor = TableEditor(
     row_factory = row_data )
 
 # The class to be edited with the TableEditor:
-class Department ( HasStrictTraits ):
-
+class Application ( HasStrictTraits ):
+    rows = List( row_data )
     aplot = Instance( HPlotContainer )
 
-    rows = List( row_data )
+    file_name = Str('test.png')
+
     add_row_button = Button('Add new row')
     print_rows_button = Button('Print row data')
 
+    save_button = Button('Save Plot')
+
     #@on_trait_change('rows')
     def _make_plot(self):
-        #self.rows = rows
-        #x = linspace(-14,14,100)
-        #y = sin(x)*(x**3)
+        x = [row.x for row in self.rows]
+        y = [row.y for row in self.rows]
+        z = [row.z for row in self.rows]
 
-        x = [row.age for row in self.rows]
-        y = [row.age for row in self.rows]
+#        plotdata = ArrayPlotData(x=x,y=y)
 
-        plotdata = ArrayPlotData(x=x,y=y)
+        idx = linspace(1,len(x),len(x))
+        vals = z
 
-        test_plot = Plot(plotdata)
-        test_plot.plot(("x","y"),type="line")
+        bar_width = 1.0
+        line_width = 5.0
+
+        index = ArrayDataSource(idx)
+        index_range = DataRange1D(index)
+        index_mapper = LinearMapper(range=index_range)
+        
+        value = ArrayDataSource(vals)
+        value_range = DataRange1D(value)
+        value_mapper = LinearMapper(range=value_range)
+
+        test_plot = BarPlot(index=index, value=value,
+                        value_mapper = value_mapper,
+                        index_mapper=index_mapper,
+                        bgcolor="white",
+                        line_color="black",
+                        fill_color="cornflowerblue",
+                        bar_width=bar_width,
+                        line_width=line_width)
+
+        #test_plot = Plot(plotdata)
+        #test_plot.plot(("x","y"),type="scatter")
+        # changing type to line will make the 
+        # plot a line plot! not recommended!
 
         container = HPlotContainer(test_plot)
         self.aplot = container
+
+    def _save_button_fired(self, filename='test.png'):
+        plot = self.aplot
+        plot.outer_bounds = [100,100]
+        plot.do_layout(force=True)
+        gc = PlotGraphicsContext((100,100),dpi=72)
+        gc.render_component(plot)
+        gc.save(filename)
 
     def _rows_changed(self):
         print 1
         self._make_plot()
 
     def _add_row_button_fired(self):
-        self.rows.append(row_data(first_name = '0',
-                                 last_name   = '0',
-                                 age = 0)
-                                )
+        self.rows.append(row_data(x = 0,
+                                  y = 0,
+                                  z = 0)
+                        )
         self._make_plot()
 
     def _print_rows_button_fired(self):
         for row in self.rows:
-            print row.first_name, row.last_name, row.age
-        return
+            print row.x, row.y, row.z
 
     traits_view = View(
                     HGroup(
@@ -111,11 +139,12 @@ class Department ( HasStrictTraits ):
                                    'print_rows_button'
                                   ),
                              ),
-                        Group(
+                        VGroup(
                             Item('aplot',
                                 editor = ComponentEditor(),
                                 width  = 500,
-                                height = 500)
+                                height = 500),
+                            Item('save_button')
                             )
                     ),
                     title     = 'TableEditor',
@@ -128,25 +157,25 @@ class Department ( HasStrictTraits ):
 
 # Create some rows
 rows = [
-    row_data( first_name = 'Jason',
-              last_name  = 'Smith',
-              age = 32),
-    row_data( first_name = 'Mike',
-              last_name  = 'Tollan',
-              age = 34),
-    row_data( first_name = 'Dave',
-              last_name  = 'Richards',
-              age = 42),
-    row_data( first_name = 'Lyn',
-              last_name  = 'Spitz',
-              age = 40),
-    row_data( first_name = 'Greg',
-              last_name  = 'Andrews',
-              age = 45)
+    row_data( x = 10,
+              y = 11,
+              z = 32),
+    row_data( x = 14,
+              y = 67,
+              z = 34),
+    row_data( x = 44,
+              y = 21,
+              z = 42),
+    row_data( x = 77,
+              y = 54,
+              z = 40),
+    row_data( x = 22,
+              y = 33,
+              z = 45)
 ]
 
 # Create the demo:
-demo = Department()
+demo = Application()
 demo.rows = rows
 
 # Run the demo (if invoked from the command line):
