38c29084a5fc7515c617c44f488ad82cd70a8252
rahul.poruri <rahul.poruri@gmail.com>
2016-04-04 15:50:22 +0530

    a complete make_graph code!

--- arxivstats/make_graph.py
+++ arxivstats/make_graph.py
@@ -2,14 +2,28 @@ import json
 from collections import defaultdict
 from graphcanvas.api import GraphView, graph_from_dict
 
-testDict = defaultdict(dict)
-alist = ['University of Michigan','MIT','Imperial College London','Pennsylvania State University','Universita degli Studi Roma Tre','INAF-IASF Bologna','INAF Arcetri','Rikkyo University','Columbia University','University of Leicester','Harvard-Smithsonian CfA','NASA/MSFC','ISAS','University of Kyoto']
+import urllib
+from BeautifulSoup import BeautifulStoneSoup
 
-for what in alist:
-    testDict[what] = []
-    for a in alist:
-        if a != what:
-            testDict[what].append(a)
+url = 'http://export.arxiv.org/api/query?search_query=all:astro&start=0&max_results=500'
+data = urllib.urlopen(url).read()
+soup = BeautifulStoneSoup(data)
+
+test = [tag for tag in soup.findAll('entry')]
+
+affiliationList = []
+for i in range(len(test)):
+            if test[i].findAll('arxiv:affiliation') != []:
+                                affiliationList.append([tag.string for tag in test[i].findAll('arxiv:affiliation')])
+
+testDict = defaultdict(dict)
+for alist in affiliationList:
+    for what in alist:
+        if len(testDict[what]) == 0:
+            testDict[what] = []
+        for item in alist:
+            if item != what:
+                testDict[what].append(item)
 
 #GraphView(graph=graph_from_dict(json.load(open('arxiv_data.dat')))).configure_traits()
 GraphView(graph=graph_from_dict(testDict)).configure_traits()
