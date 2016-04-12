91ce9bae3464b694f75ba0ff26f72fdb7526f486
Poruri Sai Rahul <rporuri@enthought.com>
2016-04-12 16:09:05 +0530

    a pretty network! yay!

--- arxivstats/make_graph.py
+++ arxivstats/make_graph.py
@@ -1,11 +1,19 @@
+# http://networkx.github.io/documentation/latest/examples/javascript/force.html
+# http://bl.ocks.org/mbostock/4062045
+
 import json
 from collections import defaultdict
 from graphcanvas.api import GraphView, graph_from_dict
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from networkx.readwrite import json_graph
+#import http_server
 
 import urllib
 from BeautifulSoup import BeautifulStoneSoup
 
-url = 'http://export.arxiv.org/api/query?search_query=all:astro&start=0&max_results=500'
+url = 'http://export.arxiv.org/api/query?search_query=all:astro&start=0&max_results=1000'
 data = urllib.urlopen(url).read()
 soup = BeautifulStoneSoup(data)
 
@@ -16,14 +24,30 @@ for i in range(len(test)):
             if test[i].findAll('arxiv:affiliation') != []:
                                 affiliationList.append([tag.string for tag in test[i].findAll('arxiv:affiliation')])
 
-testDict = defaultdict(dict)
-for alist in affiliationList:
-    for what in alist:
-        if len(testDict[what]) == 0:
-            testDict[what] = []
-        for item in alist:
-            if item != what:
-                testDict[what].append(item)
+#for list in affiliationList:
+#    list = list.strip(';')
 
-#GraphView(graph=graph_from_dict(json.load(open('arxiv_data.dat')))).configure_traits()
-GraphView(graph=graph_from_dict(testDict)).configure_traits()
+G = nx.Graph()
+
+for list in affiliationList:
+    if len(list) > 5:
+        print list
+        for pos, node1 in enumerate(list):
+            for node2 in list[pos:]:
+                G.add_edge(node1, node2)
+
+for n in G:
+        G.node[n]['name'] = n
+
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos, node_size=100, node_color='blue')
+nx.draw_networkx_edges(G, pos, edge_color='green')
+nx.draw_networkx_labels(G, pos, font_color='red')
+
+d = json_graph.node_link_data(G)
+json.dump(d, open('force.json','w'))
+print('done writing data')
+#http_server.load_url('force.html')
+
+plt.axis('off')
+plt.show()
